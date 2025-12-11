d6edb84b64ffb21f336e3a1916d5939ebbd3dbe1
JoseLopez36 <josloprui6@alum.us.es>
2025-12-11 10:17:50 +0100

    Enable multi-robot launch

--- docker/build.sh
+++ docker/build.sh
@@ -6,12 +6,14 @@ set -e
 
 # Get the directory where this script is located
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# Get the project root (parent directory of docker/)
+PROJECT_ROOT="$( cd "$SCRIPT_DIR/.." && pwd )"
 
 export DOCKER_BUILDKIT=1
 
 # Build the Docker image
 echo "Building the Docker image 'crazyflie-robotics-projects-img'..."
-if docker build --rm -t crazyflie-robotics-projects-img --file $SCRIPT_DIR/Dockerfile .; then
+if docker build --rm -t crazyflie-robotics-projects-img --file $SCRIPT_DIR/Dockerfile $PROJECT_ROOT; then
     echo "Image 'crazyflie-robotics-projects-img' built successfully."
 else
     echo "Failed to build the Docker image 'crazyflie-robotics-projects-img'."
